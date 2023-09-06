369677ad0141d9c5eaa04db79605e59a7c4e79f3
Valentino Maffeis <valentinomaffeis02@gmail.com>
2023-09-06 16:45:35 -0300

    exercise 2

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -13,6 +13,12 @@ def maximo_encadenado(a: float, b: float, c: float) -> float:
 
     Referencia: https://docs.python.org/3/reference/expressions.html#comparisons # noqa: E501
     """
+    if b<a>c:
+        return a
+    if a<c>b:
+        return c
+    return b
+        
 
 
 # NO MODIFICAR - INICIO
@@ -37,6 +43,7 @@ def maximo_cuadruple(a: float, b: float, c: float, d: float) -> float:
     """Re-escribir para que tome 4 parámetros, utilizar la función max.
 
     Referencia: https://docs.python.org/3/library/functions.html#max"""
+    return max(a,b,c,d)
 
 
 # NO MODIFICAR - INICIO
@@ -54,6 +61,8 @@ def maximo_arbitrario(*args) -> float:
     """Re-escribir para que tome una cantidad arbitraria de parámetros.
     Referencia: https://docs.python.org/3/tutorial/controlflow.html#arbitrary-argument-lists # noqa: E501
     """
+    return max(args)
+
 
 
 # NO MODIFICAR - INICIO
